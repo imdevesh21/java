3c19821f41712b65efd584a439160ac3efcbc5f9
deves <deveshsharma.it23@jecrc.com>
2024-03-26 12:26:41 +0530

    Your commit message here

---
+++ 8_Arrays/Arrays_chapter_code/Searching_in_Arrays/PairsInArray.java
@@ -0,0 +1,17 @@
+public class PairsInArray {
+
+    public static void printPair(int numbers[]){
+        for(int i = 0; i < numbers.length; i++){
+            int curr = numbers[i];
+            for(int j = i+1; j < numbers.length; j++){
+                System.out.println("(" + curr + "," + numbers[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int number[] = {2,4,5,8,10};
+        printPair(number);
+    }
+}
